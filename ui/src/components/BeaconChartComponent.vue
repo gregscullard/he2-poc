731980277fe595770de883b0c44d7e8a7bd428ab
Greg Scullard <greg@swirldslabs.com>
2022-09-15 11:55:38 +0200

    resize bar chart

--- ui/src/components/BeaconChartComponent.vue
+++ ui/src/components/BeaconChartComponent.vue
@@ -1,15 +1,17 @@
 <template>
-  <Bar
-      :chart-options="chartOptions"
-      :chart-data="chartData"
-      :chart-id="chartId"
-      :dataset-id-key="datasetIdKey"
-      :plugins="plugins"
-      :css-classes="cssClasses"
-      :styles="styles"
-      :width="width"
-      :height="height"
-  />
+  <div class="container">
+    <Bar
+        :chart-options="chartOptions"
+        :chart-data="chartData"
+        :chart-id="chartId"
+        :dataset-id-key="datasetIdKey"
+        :plugins="plugins"
+        :css-classes="cssClasses"
+        :styles="styles"
+        height=300
+        width="100%"
+    />
+  </div>
 </template>
 
 <script>
