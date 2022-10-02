6205af593b185085341e6d02479d006e844292f5
Greg Scullard <greg@swirldslabs.com>
2022-10-02 21:33:31 +0200

    Refactor simulator module

--- OracleAndHotspotSimulator/simulator/src/main/java/com/hedera/he2poc/simulator/App.java
+++ OracleAndHotspotSimulator/simulator/src/main/java/com/hedera/he2poc/simulator/App.java
@@ -1,7 +1,5 @@
 package com.hedera.he2poc.simulator;
 
-import com.hedera.he2poc.common.Secrets;
-import com.hedera.he2poc.common.balancechecker.BalanceChecker;
 import com.hedera.he2poc.common.yamlconfig.YamlConfigManager;
 import com.hedera.he2poc.hotspot.Hotspots;
 import lombok.extern.log4j.Log4j2;
@@ -16,13 +14,14 @@ public class App {
         if (args.length > 0) {
             numHotspots = Integer.parseInt(args[0]);
         }
+        log.warn("Starting simulator with {} hotspots", numHotspots);
         app.start(numHotspots);
     }
 
     public void start(int numHotspots) throws Exception {
+        boolean demo = false;
 
-        YamlConfigManager yamlConfigManager = new YamlConfigManager();
-        Secrets secrets = new Secrets();
+        YamlConfigManager yamlConfigManager = new YamlConfigManager(demo);
         if (yamlConfigManager.getHotspots().isEmpty()) {
             log.error("No hotspot details found in config.yaml");
             return;
@@ -31,14 +30,15 @@ public class App {
             log.error("No topic configuration found in config.yaml");
             return;
         }
-        Hotspots hotspots = new Hotspots();
-
-        BalanceChecker balanceChecker = new BalanceChecker(yamlConfigManager.getHotspotAccountIds(), secrets.privateKey(), secrets.accountId(), secrets.network());
-        Thread balanceCheckerThread = new Thread(balanceChecker);
-        balanceCheckerThread.start();
+        Hotspots hotspots = new Hotspots(demo);
 
         if (numHotspots == 0) {
             numHotspots = yamlConfigManager.getHotspots().size();
+        }
+        if (numHotspots > yamlConfigManager.getHotspots().size()) {
+            log.warn("Number of hotspots {} exceeds pre-configured hotspot list size", numHotspots);
+            numHotspots = yamlConfigManager.getHotspots().size();
+            log.warn("Running with {} hotspots ", numHotspots);
         }
         for (int i=1; i <= numHotspots; i++) {
             hotspots.startHotspot(i);
